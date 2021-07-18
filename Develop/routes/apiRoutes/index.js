aceb6ddb5b99644d936a0f2a90f73c751c9bbe06
Troy Grossi <ttroygrossit@gmail.com>
2021-07-18 11:11:14 -0700

    linked javascript fetch with post and get routes

--- Develop/routes/apiRoutes/index.js
+++ Develop/routes/apiRoutes/index.js
@@ -1,12 +1,35 @@
 const router = require("express").Router();
 const path = require("path");
-const { notes } = require(path.join(__dirname, "../../db/db.json"));
+const fs = require("fs");
+const { notes } = require("../../db/db.json");
 
-router.get("/notes", (req, res) => {
-  res.send(notes);
+router.get("/api/notes", (req, res) => {
+  res.json(notes);
 });
 
-router.post("/notes", (req, res) => {
+router.get("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  const noteById = notes.filter((notes) => {
+    if (parseInt(note.id) === id) {
+      return note;
+    }
+  });
+  res.json(noteById);
+});
+
+router.post("/api/notes", (req, res) => {
+  const newNotes = req.body;
+  newNotes.id = notes.length.toString();
+  notes.push(newNotes);
+  fs.writeFile(
+    path.join(__dirname, "../../db/db.json"),
+    JSON.stringify({ notes: notes }, null, 2),
+    (err) => {
+      if (err) {
+        throw new Error(err);
+      }
+    }
+  );
   res.json(notes);
 });
 
--- Develop/server.js
+++ Develop/server.js
@@ -10,8 +10,8 @@ app.use(express.static("public"));
 const htmlRoutes = require("./routes/htmlRoutes/index.js");
 const apiRoutes = require("./routes/apiRoutes/index.js");
 
-app.use("/", htmlRoutes);
-app.use("/", apiRoutes);
+app.use("", htmlRoutes);
+app.use("", apiRoutes);
 
 app.listen(PORT, () => {
   console.log("API server now on port " + PORT);
